04f461071f3335fc9fdc121eb16129b827ee7acf
Rus <de.mr44@gmail.com>
2021-11-17 23:11:46 +0500

    Added Basket class

---
+++ Multithreading-Threads/Basket.swift
@@ -0,0 +1,28 @@
+//
+//  Basket.swift
+//  Multithreading-Threads
+//
+//  Created by ruslan on 17.11.2021.
+//
+
+import Foundation
+
+// thread safe array with mutex
+final class Basket {
+    
+    private let mutex = NSLock()
+    private(set) var array: [Bread] = []
+    
+    func put(_ object: Bread) {
+        mutex.lock(); defer { mutex.unlock() }
+        array.append(object)
+    }
+    
+    func take() -> Bread {
+        guard array.isEmpty == false else { fatalError("Busket is empty!") }
+        mutex.lock(); defer { mutex.unlock() }
+        let lastObject = array.removeLast()
+        
+        return lastObject
+    }
+}
